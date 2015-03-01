1606d9952dd104e9cad167cc2c41205cc4e4bdaf
安藤　慶子 <keando@MBP-15JAC-370.local>
2015-03-01 20:49:05 +0900

    画像追加

--- Rainboo/ViewController.swift
+++ Rainboo/ViewController.swift
@@ -20,6 +20,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func buttonTouched(sender: AnyObject) {
+        println("ボタン押された")
+        
+        rainboo.hidden = false
+        
+        
 
 }
 
